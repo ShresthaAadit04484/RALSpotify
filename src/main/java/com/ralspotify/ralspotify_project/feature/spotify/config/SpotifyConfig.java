e278ba4889ab80b0f3dde9b928411c303b3c760e
Aadit <shresaadit@gmail.com>
2025-02-02 17:38:54 +0545

    Fix: JWT token fix for the JWT authentication.

---
+++ src/main/java/com/ralspotify/ralspotify_project/feature/spotify/config/SpotifyConfig.java
@@ -0,0 +1,22 @@
+package com.ralspotify.ralspotify_project.feature.spotify.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpotifyConfig {
+    public SecurityFilterChain spotifySecurityFilterChain(HttpSecurity http) throws Exception{
+        http 
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/login/oauth2/**", "/oauth2/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .oauth2Login(oauth -> oauth
+                .defaultSuccessUrl("/home", true)
+            );
+        return http.build();
+    }
+}
